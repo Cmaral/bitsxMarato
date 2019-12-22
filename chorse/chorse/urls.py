7a96f0025eaffc7a196999076bfaf2d8cee36c89
xarxax <xarxax.dev@gmail.com>
2019-12-22 02:16:51 +0100

    changed default

--- chorse/chorse/urls.py
+++ chorse/chorse/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include,re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('diagnoser.urls')),
+    re_path(r'.*', include('diagnoser.urls')),
     path('diagnoser/', include('diagnoser.urls')),
 
 ]
